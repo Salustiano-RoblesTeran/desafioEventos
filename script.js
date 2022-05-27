aaa6c6290cdb7e284eab2895609fdce5cd3e5fc1
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2022-05-26 23:55:22 -0300

    Subiendo desafio

---
+++ script.js
@@ -0,0 +1,11 @@
+let pan = document.getElementById("pan");
+let fernet = document.getElementById("fernet");
+let coca = document.getElementById("coca");
+let yerba = document.getElementById("yerba");
+
+
+let carrito = document.createElement("container-carrito");
+
+pan.addEventListener('input', () => {
+    console.log("Agrego pan");
+})
